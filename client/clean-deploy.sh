4834f7ce547c3493c1798cc3165128cc6b2bc44a
Raphael Campos <raphaelcampos01@proton.me>
2024-10-07 11:03:49 -0300

    clean

--- client/clean-deploy.sh
+++
@@ -1,4 +0,0 @@
-rm -r dist
-rm -r node_modules
-rm package.json
-rm -r .wrangler
--- client/deploy-spa.sh
+++
@@ -1,6 +0,0 @@
-gleam run -m lustre/dev build app --minify
-mkdir -p dist
-cp index.html dist/index.html
-cp -r priv dist/priv
-npm install wrangler --save-dev
-npx wrangler pages deploy dist --project-name lauraxv-14dez
